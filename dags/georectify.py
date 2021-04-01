dc659fbf880000c07d2a6890c8748d54e42c108b
Mattia Giupponi <mattia.giupponi@gmail.com>
2021-04-01 12:25:57 +0200

    Georectify operator call geonode with example json

--- dags/georectify.py
+++ dags/georectify.py
@@ -9,37 +9,35 @@ import os
 
 
 args = {
-    'owner': 'airflow',
+    "owner": "airflow",
 }
 
-upload_dir = Variable.get('UPLOAD_DIR', '/opt/uploads')
-output_dir = Variable.get('OUTPUT_DIR', '/opt/output')
-geonode_endpoint = Variable.get('GEONODE_ENDPOINT', 'localhost')
+upload_dir = Variable.get("UPLOAD_DIR", "/opt/uploads")
+output_dir = Variable.get("OUTPUT_DIR", "/opt/output")
 
 
-def create_pipeline(dag_id,
-               schedule,
-               default_args,
-               abs_filepath):
+def create_pipeline(dag_id, schedule, default_args, abs_filepath, filename):
 
-    dag = DAG(dag_id,
-              schedule_interval=schedule,
-              default_args=default_args)
+    dag = DAG(
+        dag_id, schedule_interval=schedule, default_args=default_args, max_active_runs=1
+    )
 
     with dag:
         georectify = GeoRectifyOperator(
-            task_id='geoprocessing',
+            task_id="geoprocessing",
             abs_filepath=abs_filepath,
             default_args=default_args,
             output_folder=output_dir,
             dag=dag,
+            filename=filename
         )
         update_layers = SimpleHttpOperator(
-            task_id='update_layer',
-            http_conn_id='geonode_conn_id',
-            endpoint=geonode_endpoint,
-            method='GET',
-            data="{{ ti.xcom_pull(task_ids='geoprocessing')}}"
+            task_id="update_layer",
+            http_conn_id="geonode_conn_id",
+            endpoint="api/management/importlayers/",
+            method="POST",
+            headers={"Content-Type": "application/json"},
+            data="{{ ti.xcom_pull(task_ids='geoprocessing')}}",
         )
 
         georectify >> update_layers
@@ -47,11 +45,18 @@ def create_pipeline(dag_id,
     return dag
 
 
-for item in [i for i in os.listdir(upload_dir) if i.endswith('tif') and '_fin' not in i]:
-    default_args = {
-        'owner': 'airflow',
-        'start_date': datetime(2020, 3, 30)
-    }
-    name = re.sub('\.tif$', '', item)
+for item in [i for i in os.listdir(upload_dir) if i.endswith("tif") and "_fin" not in i]:
+
+    default_args = {"owner": "airflow", "start_date": datetime(2020, 3, 30)}
+
+    name = re.sub("\.tif$", "", item)
+
     abs_filepath = f"{upload_dir}/{item}"
-    globals()[item] = create_pipeline(f'{name}', '@once', default_args, abs_filepath)
+
+    globals()[item] = create_pipeline(
+        dag_id=f"{name}",
+        schedule="@once",
+        default_args=default_args,
+        abs_filepath=abs_filepath,
+        filename=item,
+    )
--- plugins/custom_operator/georectify.py
+++ plugins/custom_operator/georectify.py
@@ -1,28 +1,44 @@
+import re
 from airflow.models.baseoperator import BaseOperator
 from airflow.utils.decorators import apply_defaults
 from geotiflib.georectify import GeoRectifyFactory
 from geotiflib.geotiff import GeoTiff
-class GeoRectifyOperator(BaseOperator):
 
+
+class GeoRectifyOperator(BaseOperator):
     @apply_defaults
-    def __init__(
-            self,
-            abs_filepath: str,
-            output_folder: str,
-            **kwargs):
+    def __init__(self, abs_filepath: str, output_folder: str, filename:str, **kwargs):
         super().__init__(**kwargs)
         self.abs_filepath = abs_filepath
         self.output_folder = output_folder
+        self.filename = filename
 
     def execute(self, context):
         process = GeoRectifyFactory.create(
-            input=self.abs_filepath, 
-            qgis_scripts='/usr/bin/',
-            output_folder=self.output_folder
-            )
+            input=self.abs_filepath,
+            qgis_scripts="/usr/bin/",
+            output_folder=self.output_folder,
+        )
+
         def on_progress(message):
             print(str(message["value"]))
+
         process.on_progress += on_progress
         process.process()
         info = GeoTiff(self.abs_filepath).info()
-        return info
+        name = re.sub("\.tif$", "", self.filename)
+
+        geonode_json = {
+            "args": [
+                f"{self.output_folder}/{self.filename}",
+                "--overwrite",
+            ],
+            "kwargs": {
+                "user": "admin",
+                "name": name,
+                "title": "imported layer updated",  # get from geotiff lib
+                "regions": "global", # get from geotiff lib
+                "keywords": "key89,key344", # get from geotiff lib
+            },
+        }
+        return geonode_json
